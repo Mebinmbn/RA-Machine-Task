92d760c49c7a172122af0ae9e3df68d6ff04b39d
Mebin Joseph <mebinmbn.joseph04@gmail.com>
2025-04-18 22:44:09 +0530

    Home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Home() {
-  return <div className="h-screen"></div>;
+  return (
+    <div className="relative overflow-hidden text-white h-screen">
+      <div className="fixed inset-0 -z-10 animated-gradient "></div>
+    </div>
+  );
 }
 
 export default Home;
